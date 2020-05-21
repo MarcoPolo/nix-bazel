f1650b5a0a3ff4cf9222864704c4e7a24c471939
Marco Munizaga <git@marcopolo.io>
2020-05-21 12:33:33 -0700

    First

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Docker image that lives on [marcop010/nix-bazel](https://hub.docker.com/repository/docker/marcop010/nix-bazel)
+
+## Why?
+
+Bazel + Nix is a great tool for hermetic reproducible builds.
---
+++ default.nix
@@ -0,0 +1,18 @@
+{ system ? builtins.currentSystem, ... }@config:
+let
+  pkgs = import <nixpkgs> config;
+  bazel3 = (import (builtins.fetchTarball
+    "https://github.com/avdv/nixpkgs/archive/bazel_3.tar.gz") config).bazel;
+in with pkgs;
+pkgs.dockerTools.buildImage {
+  name = "marcop010/nix-bazel";
+  tag = "latest";
+  # contents = [ bash nix bazel3 ];
+  contents = [ bash nix ];
+
+  config = {
+    Cmd = [ "/bin/bash" ];
+    WorkingDir = "/app";
+    # Volumes = { "/data" = { }; };
+  };
+}

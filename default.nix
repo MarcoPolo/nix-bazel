8cef9290c1943ae6bc2bb7b36f57950aa7b58c16
Marco Munizaga <git@marcopolo.io>
2020-05-21 14:50:41 -0700

    Base it off nixos/nix image

--- default.nix
+++ default.nix
@@ -3,11 +3,18 @@ let
   pkgs = import <nixpkgs> config;
   bazel3 = (import (builtins.fetchTarball
     "https://github.com/avdv/nixpkgs/archive/bazel_3.tar.gz") config).bazel;
-in with pkgs;
+  nixos = pkgs.dockerTools.pullImage {
+    imageName = "nixos/nix";
+    finalImageTag = "latest";
+    imageDigest = "sha256:399eb8b7faf90fd010674da906366bbb2391f04577c0365a4d7d9300678e84c1";
+    sha256 = "sha256:08cnjwl5jh35pngbq9rkvj5mkk7d3m3k0hfxhr6v4i63brm8n2s3";
+  };
+in
 pkgs.dockerTools.buildImage {
   name = "marcop010/nix-bazel";
   tag = "latest";
-  contents = [ bash nix bazel3 coreutils ];
+  fromImage = nixos;
+  contents = with pkgs; [ bazel3 ];
 
   config = {
     Cmd = [ "/bin/bash" ];

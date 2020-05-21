361d6bbfd91559f8ea1e8de90b75e4ace803d709
Marco Munizaga <git@marcopolo.io>
2020-05-21 15:01:57 -0700

    Base it off nixos/nix image

--- default.nix
+++ default.nix
@@ -17,7 +17,15 @@ pkgs.dockerTools.buildImage {
   contents = with pkgs; [ bazel3 ];
 
   config = {
-    Cmd = [ "/bin/bash" ];
+    Cmd = [ "/bin/sh" ];
     WorkingDir = "/app";
+    Env = [
+      "ENV=/etc/profile"
+      "USER=root"
+      "PATH=/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin"
+      "GIT_SSL_CAINFO=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt"
+      "NIX_SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt"
+      "NIX_PATH=/nix/var/nix/profiles/per-user/root/channels"
+    ];
   };
 }
